a397d48f85a28a11789e199af078a9b1e6e5e202
6P_kimdongmin <6P_kimdongmin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-05-20 07:01:44 +0000

    Feature - StoryComponentNetwork
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@118 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/StoryComponentNetwork/Plugins/Object_Plugins/Source/Object_Plugins/Public/SzInterface/Hacking.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Hacking.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UHacking : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class OBJECT_PLUGINS_API IHacking
+{
+	GENERATED_BODY()
+
+public:
+	// 해킹 실행 (E키 홀딩)
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Hacking")
+	void StartHacking();
+
+	// 해킹 완료 후 로직 (CCTV 보임, 적 무력화 등)
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Hacking")
+	void FinishHacking();
+
+	// 해킹 가능 여부 체크 (false => 해킹 완료 / true => 해킹 전)
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Hacking")
+	bool CanBeHacked() const;
+
+	// 클리어
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Hacking")
+	void ClearHoldingTimer() const;
+};
