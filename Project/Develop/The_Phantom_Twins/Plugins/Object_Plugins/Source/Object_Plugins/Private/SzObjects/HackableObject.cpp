b3d1a6888a788e8c26115d2450f644a468e0c322
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-05-20 04:11:39 +0000

    Merged Object_Hacking
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@117 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Develop/The_Phantom_Twins/Plugins/Object_Plugins/Source/Object_Plugins/Private/SzObjects/HackableObject.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SzObjects/HackableObject.h"
+#include "Blueprint/UserWidget.h"
+#include "SzComponents/HackableComponent.h"
+
+// Sets default values
+AHackableObject::AHackableObject()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	bIsHacked = false;
+}
+
+// Called when the game starts or when spawned
+void AHackableObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHackableObject::StartHacking_Implementation()
+{
+	UE_LOG(LogTemp, Log, TEXT("StartHacking"));
+
+	if (bIsHacked) return;
+
+	// RequiredTime 동안 E키를 눌렀으면 bIsHacked = true
+	// RequiredTime이 되기 전에 E키를 뗐으면 bIsHacked = false
+	//GetWorldTimerManager.SetTimer(HackingTimer, )
+}
+
+void AHackableObject::FinishHacking_Implementation()
+{
+	UE_LOG(LogTemp, Log, TEXT("FinishHacking"));
+
+	// 해킹 컴포넌트 확인
+	if (HackingComp)
+	{
+		HackingComp->Execute();
+	}
+
+	//// HackedDuration 시간이 지나면 다시 해킹 전 상태(bIsHacked = false)로 바꿈
+	//GetWorldTimerManager.SetTimer(HackedTimer, )
+
+}
+
+bool AHackableObject::CanBeHacked_Implementation() const
+{
+	// 플레이어의 코어 에너지가 없으면 false(나중에는 player->EnergyNum <=0 으로 비교)
+	if (RequiredEnergy <= 0)
+	{
+		return false;
+	}
+
+	return !bIsHacked;	// 해킹된 상태랑 해킹할 수 있는 상태는 반대.
+}
+
+void AHackableObject::ClearHoldingTimer_Implementation()
+{
+	UE_LOG(LogTemp, Log, TEXT("ClearHoldingTimer"));
+}
+
+void AHackableObject::OnHackingSucceeded()
+{
+
+}
+
+void AHackableObject::OnHackingExpired()
+{
+
+}
