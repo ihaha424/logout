b0926dc4a13c08ff645c904d5639d298eb085fa9
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-05-25 14:25:51 +0000

    [Object_Camera] copy
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@168 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/Object_Camera/Source/The_Phantom_Twins/AI/MyAICharacter.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SplinePathActor.h"
+#include "MyAICharacter.generated.h"
+
+UCLASS()
+class THE_PHANTOM_TWINS_API AMyAICharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyAICharacter();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Spline")
+	ASplinePathActor* SplinePath;
+
+	float DistanceAlongSpline = 0.f;
+	bool bMovingForward = true;
+
+	UPROPERTY(EditAnywhere, Category = "AI|Spline")
+	float MoveSpeed = 200.f;
+
+	bool bIsFollowingSpline = false;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	ASplinePathActor* GetSplinePath() const { return SplinePath; }
+};
