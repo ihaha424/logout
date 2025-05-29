13dd99786c9cc444ec52fbaca8868711815752e8
6P_kimdongmin <6P_kimdongmin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-05-29 05:44:36 +0000

    Feature/StoryFlow
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@170 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/StoryFlow/Source/The_Phantom_Twins/AI/MyAIController.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
+#include "MyAIController.generated.h"
+
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+class UAISenseConfig_Hearing;
+class UBehaviorTree;
+class UBlackboardData;
+/**
+ * 
+ */
+UCLASS()
+class THE_PHANTOM_TWINS_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+public:
+	AMyAIController();
+
+	void RunAI();
+	void StopAI();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* InPawn) override;
+
+	// 감지 콜백 함수
+	UFUNCTION()
+	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "AI")
+	TObjectPtr<UAIPerceptionComponent> AIPerception;
+
+	UPROPERTY()
+	TObjectPtr<UAISenseConfig_Sight> SightConfig;
+
+	UPROPERTY()
+	TObjectPtr<UAISenseConfig_Hearing> HearingConfig;
+
+	// 비헤이비어 트리와 블랙보드
+	UPROPERTY()
+	TObjectPtr<UBehaviorTree> BTAI;
+
+	UPROPERTY()
+	TObjectPtr<UBlackboardData> BBAI;
+};
