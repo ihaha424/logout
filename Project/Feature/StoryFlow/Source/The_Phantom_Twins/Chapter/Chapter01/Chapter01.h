228b5e8055a00c4d877790ad976935e7bd524cac
6P_kimdongmin <6P_kimdongmin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-05-30 13:55:19 +0000

    [Feature - StoryFlow] StoryFlowManager, PhantomTwinsInstance, SaveState, ChaptorData (+ StoryFlowData Example)
    1. StoryFlowManager 테스트용
    2. PhantomTwinsInstance(GameInstance) 제작
    3. SaveState 테스트용
    4. ChaptorData 제작
    5. StoryFlowManager 사용법 예제 제작
    
    
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@171 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/StoryFlow/Source/The_Phantom_Twins/Chapter/Chapter01/Chapter01.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Chapter01.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THE_PHANTOM_TWINS_API UChapter01 : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
