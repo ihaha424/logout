367a57c74fb5acec9d5f7d6c721af95f1e6b845e
6P_kimdongmin <6P_kimdongmin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-06-10 11:35:16 +0000

    [Feature-StoryFlow] OutGame
    1. MainMenu GS,BaseWidget , BP로 Connect 연결 - 이후 ClientConnect로 좀 더 수정해야함
    2. Chaptor에 쓰이는 PC 시작시 입력 모드 수정(UI-X,Game-O)
    
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@301 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/StoryFlow/Source/The_Phantom_Twins/OutGame/Lobby/GM_Lobby.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Lobby.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THE_PHANTOM_TWINS_API ALobbyGM : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Project/Feature/StoryFlow/Source/The_Phantom_Twins/OutGame/Lobby/GS_Lobby.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "GS_Lobby.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THE_PHANTOM_TWINS_API ALobbyGS : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
