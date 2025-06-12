c974cfce711267906fb69552fd20dface516b1f3
6P_hamshina <6P_hamshina@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-06-12 15:31:31 +0000

    [Feature] Commit Enemy_SwitchSplinePath
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@324 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/Enemy_SwitchSplinePath/Source/The_Phantom_Twins/AI/BTT_PatrolStimulusLocation.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_PatrolStimulusLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THE_PHANTOM_TWINS_API UBTT_PatrolStimulusLocation : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	UBTT_PatrolStimulusLocation();
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float MoveSpeed = 200.f;
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float AcceptanceRadius = 5.f;
+
+private:
+	int32 CurrentIndex = 0;
+	int32 MaxIndex = 0;
+	FVector TargetLocation;
+};
