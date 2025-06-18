678a75ff3b36e2003346d49e74d934e01b76fda0
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-06-18 09:13:48 +0000

    [Object_Hide] HideComponent client 움직이는 버그 수정
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@479 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/Object_Hide/The_Phantom_Twins/Plugins/Object_Plugins/Source/Object_Plugins/Private/SzComponents/HideComponent.cpp
+++ Project/Feature/Object_Hide/The_Phantom_Twins/Plugins/Object_Plugins/Source/Object_Plugins/Private/SzComponents/HideComponent.cpp
@@ -1,7 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "SzComponents/HideComponent.h"
+﻿#include "SzComponents/HideComponent.h"
 #include "SzObjects/BaseObject.h"
 #include "Components/BoxComponent.h"
 #include "GameFramework/Pawn.h"
@@ -14,15 +11,19 @@
 #include "Net/UnrealNetwork.h"
 
 
+#include "Kismet/KismetSystemLibrary.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogHideComponent, Log, All);
+
 // Sets default values for this component's properties
 UHideComponent::UHideComponent()
 {
-	PrimaryComponentTick.bCanEverTick = false;
+    PrimaryComponentTick.bCanEverTick = false;
 
-	TriggerComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxTriggerComponent"));
-	TriggerComponent->SetupAttachment(this);
-	TriggerComponent->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
-	TriggerComponent->SetGenerateOverlapEvents(true);
+    TriggerComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxTriggerComponent"));
+    TriggerComponent->SetupAttachment(this);
+    TriggerComponent->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+    TriggerComponent->SetGenerateOverlapEvents(true);
 
     SetIsReplicatedByDefault(true); // 컴포넌트 복제 활성화
 }
@@ -32,47 +33,44 @@ void UHideComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLi
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
     DOREPLIFETIME(UHideComponent, bHasPlayer);
+    DOREPLIFETIME(UHideComponent, bUseCamera);
+    DOREPLIFETIME(UHideComponent, HidePlayer);
+    DOREPLIFETIME(UHideComponent, bIsInHideView);
+    DOREPLIFETIME(UHideComponent, PreviousViewTarget);
 }
 
 // Called when the game starts
 void UHideComponent::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	if (!TriggerComponent)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TriggerComponent is null in HideComponent"));
-		return;
-	}
+    if (!TriggerComponent)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("TriggerComponent is null in HideComponent"));
+        return;
+    }
 
-	AActor* Owner = GetOwner();
+    AActor* Owner = GetOwner();
 
-	if (Owner)
-	{
-		UStaticMeshComponent* MeshComp = Owner->FindComponentByClass<UStaticMeshComponent>();
-		if (MeshComp && MeshComp->GetStaticMesh()) {
-			FVector Extent = MeshComp->GetStaticMesh()->GetBounds().BoxExtent;
-			TriggerComponent->SetBoxExtent(Extent);
-		}
-	}
+    if (Owner)
+    {
+        UStaticMeshComponent* MeshComp = Owner->FindComponentByClass<UStaticMeshComponent>();
+        if (MeshComp && MeshComp->GetStaticMesh()) {
+            FVector Extent = MeshComp->GetStaticMesh()->GetBounds().BoxExtent;
+            TriggerComponent->SetBoxExtent(Extent);
+        }
+    }
 
-	TriggerComponent->OnComponentBeginOverlap.AddDynamic(this, &UHideComponent::OnTriggerBeginOverlap);
-	TriggerComponent->OnComponentEndOverlap.AddDynamic(this, &UHideComponent::OnTriggerEndOverlap);
+    TriggerComponent->OnComponentBeginOverlap.AddDynamic(this, &UHideComponent::OnTriggerBeginOverlap);
+    TriggerComponent->OnComponentEndOverlap.AddDynamic(this, &UHideComponent::OnTriggerEndOverlap);
 }
 
 void UHideComponent::ExecuteSever(APawn* Interactor)
 {
     // 플레이어 컨트롤러 가져오기
-    APlayerController* PlayerController = Cast<APlayerController>(Interactor->GetController());// UGameplayStatics::GetPlayerController(GetWorld(), 0);
-    if (!PlayerController)
-    {
-        UE_LOG(LogTemp, Error, TEXT("PlayerController is null in HideComponent::Execute"));
-        return;
-    }
+    APlayerController* PlayerController = CastChecked<APlayerController>(Interactor->GetController());
 
-    if (nullptr != HidePlayer && HidePlayer != PlayerController)
-        return;
-
+    if (nullptr != HidePlayer && HidePlayer != PlayerController) return;
 
     // 오너 액터 가져오기 (BP_HideCamChange)
     AActor* OwnerActor = GetOwner();
@@ -85,7 +83,6 @@ void UHideComponent::ExecuteSever(APawn* Interactor)
     // 카메라 컴포넌트 찾기 (처음 실행 시)
     if (bUseCamera && !HideCamera)
     {
-
         HideCamera = OwnerActor->FindComponentByClass<UCameraComponent>();
         if (!HideCamera)
         {
@@ -93,106 +90,176 @@ void UHideComponent::ExecuteSever(APawn* Interactor)
             return;
         }
     }
-    
-    if (bUseCamera && HideCamera)
+
+    if (bUseCamera)
     {
-        // 카메라 전환 로직
-        if (!bIsInHideView)
-        {
-            // 전환 전 입력 비활성화
-            PlayerController->SetIgnoreMoveInput(true);
-            PlayerController->SetIgnoreLookInput(true);
-
-            // 현재 뷰 타겟 저장
-            PreviousViewTarget = PlayerController->GetViewTarget();
-
-            // 오너 액터(BP_HideCamChange)의 카메라로 전환
-            PlayerController->SetViewTargetWithBlend(OwnerActor, CameraBlendTime);
-
-            // 상태 변경
-            bIsInHideView = true;
-            bHasPlayer = true;
-            HidePlayer = PlayerController;
-
-            Debug_bHasPlayer();
-
-        }
-        else
-        {
-            // 이전 뷰 타겟(BP_ThirdPersonCharacter)으로 돌아가기
-            if (PreviousViewTarget)
-            {
-                PlayerController->SetViewTargetWithBlend(PreviousViewTarget, CameraBlendTime);
-            }
-            else
-            {
-                // 이전 뷰 타겟이 없으면 플레이어의 Pawn을 사용
-                PlayerController->SetViewTargetWithBlend(Interactor, CameraBlendTime);
-            }
-
-            // 입력 다시 활성화
-            PlayerController->SetIgnoreMoveInput(false);
-            PlayerController->SetIgnoreLookInput(false);
-
-            // 상태 변경
-            bIsInHideView = false;
-            bHasPlayer = false;
-            HidePlayer = nullptr;
-
-            Debug_bHasPlayer();
-        }
+        if (bHasPlayer && HidePlayer != PlayerController) return;
+        UseCamLogic(PlayerController);
     }
     else
     {
-        // 카메라 없는 경우 기본 숨기 동작
-        UE_LOG(LogTemp, Log, TEXT("Performing basic hide action"));
-        // 여기에 추가적인 숨기 로직 작성 (예: 캐릭터 비활성화)
+        NoCamLogic(PlayerController);
+    }
+}
+
+void UHideComponent::ExecuteClient(APawn* Interactor)
+{
+    if (GetOwner()->HasAuthority()) return;
+
+    // 플레이어 컨트롤러 가져오기
+    APlayerController* PlayerController = CastChecked<APlayerController>(Interactor->GetController());
+
+    if (nullptr != HidePlayer && HidePlayer != PlayerController) return;
+
+    // 오너 액터 가져오기 (BP_HideCamChange)
+    AActor* OwnerActor = GetOwner();
+    if (!OwnerActor)
+    {
+        UE_LOG(LogTemp, Error, TEXT("OwnerActor is null in HideComponent::Execute"));
+        return;
     }
 
+    // 카메라 컴포넌트 찾기 (처음 실행 시)
+    if (bUseCamera && !HideCamera)
+    {
+        HideCamera = OwnerActor->FindComponentByClass<UCameraComponent>();
+        if (!HideCamera)
+        {
+            UE_LOG(LogTemp, Error, TEXT("No CameraComponent found in owner actor: %s"), *OwnerActor->GetName());
+            return;
+        }
+    }
+
+    if (bUseCamera)
+    {
+        if (bHasPlayer && HidePlayer != PlayerController) return;
+        ClientUseCamLogic(PlayerController);
+    }
+    else
+    {
+        NoCamLogic(PlayerController);
+    }
+}
+
+
+void UHideComponent::NoCamLogic(APlayerController* InteractorPC)
+{
+    // 카메라 없는 경우 기본 숨기 동작
+    UE_LOG(LogTemp, Log, TEXT("Performing basic hide action"));
+    // 여기에 추가적인 숨기 로직 작성 (예: 캐릭터 비활성화)
+}
+
+void UHideComponent::UseCamLogic(APlayerController* InteractorPC)
+{
+    // 서버에서 실행되는 코드
+    if (!GetOwner()->HasAuthority()) return;
+
+	if (!bIsInHideView)
+	{
+		EnterObject(InteractorPC);
+
+        // 클라이언트에게 입력 비활성화 명령 전달
+        SetInputState(InteractorPC, true);
+
+        // 클라이언트에게 카메라 전환 명령 전달
+        SetViewTarget(InteractorPC, GetOwner());
+	}
+	else
+	{
+		ExitObject(InteractorPC);
+
+        // 클라이언트에게 입력 활성화 명령 전달
+        SetInputState(InteractorPC, false);
+
+        // 클라이언트에게 카메라 전환 명령 전달
+        SetViewTarget(InteractorPC, PreviousViewTarget);
+	}
+}
+
+void UHideComponent::ClientUseCamLogic(APlayerController* InteractorPC)
+{
+    if (!bIsInHideView)
+    {
+        // 클라이언트에게 입력 비활성화 명령 전달
+        SetInputState(InteractorPC, true);
+
+        // 클라이언트에게 카메라 전환 명령 전달
+        SetViewTarget(InteractorPC, GetOwner());
+    }
+    else
+    {
+        // 클라이언트에게 입력 활성화 명령 전달
+        SetInputState(InteractorPC, false);
+
+        // 클라이언트에게 카메라 전환 명령 전달
+        SetViewTarget(InteractorPC, PreviousViewTarget);
+    }
+}
+
+void UHideComponent::EnterObject(APlayerController* InteractorPC)
+{
+    // 현재 뷰 타겟 저장
+    PreviousViewTarget = InteractorPC->GetViewTarget();
+
+    // 상태 변경
+    bIsInHideView = true;
+    bHasPlayer = true;
+    HidePlayer = InteractorPC;
+}
+
+void UHideComponent::ExitObject(APlayerController* InteractorPC)
+{
+    // 상태 변경
+    bIsInHideView = false;
+    bHasPlayer = false;
+    HidePlayer = nullptr;
+}
+
+
+// 클라이언트에서 입력 제어를 위한 RPC 함수 구현
+void UHideComponent::SetInputState(APlayerController* InteractorPC, bool bIgnoreInput)
+{
+    UKismetSystemLibrary::PrintString(this, "S2C_SetInputState_Implementation");
+
+    if (InteractorPC && InteractorPC->IsLocalController())
+    {
+        InteractorPC->SetIgnoreMoveInput(bIgnoreInput);
+        InteractorPC->SetIgnoreLookInput(bIgnoreInput);
+
+        UE_LOG(LogTemp, Log, TEXT("Client: SetIgnoreInput called with value: %s"),
+            bIgnoreInput ? TEXT("True") : TEXT("False"));
+    }
+}
+
+// 클라이언트에서 카메라 전환을 위한 RPC 함수 구현
+void UHideComponent::SetViewTarget(APlayerController* InteractorPC, AActor* NewViewTarget)
+{
+    ULocalPlayer* LocalPlayer = InteractorPC->GetLocalPlayer();
+    //UE_LOG(LogHideComponent, Log, TEXT("LocalPlayer = %s"), *LocalPlayer->GetName());
+
+	if (InteractorPC && NewViewTarget)
+	{
+		InteractorPC->SetViewTargetWithBlend(NewViewTarget, CameraBlendTime);
+
+		UE_LOG(LogTemp, Log, TEXT("Client: SetViewTarget called with actor: %s"),
+			*NewViewTarget->GetName());
+	}
 }
 
 void UHideComponent::OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	APawn* Pawn = Cast<APawn>(OtherActor);
-	if (Pawn && !bUseCamera)
-	{
-		bHasPlayer = true;
-	}
-
-    Debug_bHasPlayer();
-
+    APawn* Pawn = Cast<APawn>(OtherActor);
+    if (Pawn && !bUseCamera)
+    {
+        bHasPlayer = true;
+    }
 }
 
 void UHideComponent::OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	APawn* Pawn = Cast<APawn>(OtherActor);
-	if (Pawn && !bUseCamera)
-	{
-		bHasPlayer = false;
-	}
-
-    Debug_bHasPlayer();
-}
-
-void UHideComponent::Debug_bHasPlayer()
-{
-    // 로그 출력
-    if (GEngine)
+    APawn* Pawn = Cast<APawn>(OtherActor);
+    if (Pawn && !bUseCamera)
     {
-        FString RoleString = GetOwner()->HasAuthority() ? TEXT("Server") : TEXT("Client");
-        ENetMode NetMode = GetNetMode();
-        FString NetModeString;
-        switch (NetMode)
-        {
-        case NM_Standalone: NetModeString = TEXT("Standalone"); break;
-        case NM_ListenServer: NetModeString = TEXT("ListenServer"); break;
-        case NM_Client: NetModeString = TEXT("Client"); break;
-        default: NetModeString = TEXT("Unknown"); break;
-        }
-        GEngine->AddOnScreenDebugMessage(
-            -1, 5.f, FColor::Yellow,
-            FString::Printf(TEXT("[EndOverlap] %s (%s): bHasPlayer = %s"),
-                *RoleString, *NetModeString, bHasPlayer ? TEXT("True") : TEXT("False"))
-        );
+        bHasPlayer = false;
     }
 }
