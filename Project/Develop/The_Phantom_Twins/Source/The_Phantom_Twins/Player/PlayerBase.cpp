cc25365aa39c37f3d16c144f50178749c4bdca13
6P_kimdongmin <6P_kimdongmin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-06-25 02:15:10 +0000

    [HotFix] Recovery & Restart
    현재 Recovery가 되어도 Dead판정으로 문제 발생 -> 수정
    
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@564 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Develop/The_Phantom_Twins/Source/The_Phantom_Twins/Player/PlayerBase.cpp
+++ Project/Develop/The_Phantom_Twins/Source/The_Phantom_Twins/Player/PlayerBase.cpp
@@ -765,7 +765,7 @@ void APlayerBase::SetRecovery()
 	{
 		if (AGM_PhantomTwins* GM = GetWorld()->GetAuthGameMode<AGM_PhantomTwins>())
 		{
-			GM->NotifyPlayerDied(GetController(), true);
+			GM->NotifyPlayerDied(GetController(), false);
 		}
 	}
 }
