20ed485b3a42550eed94ff2ebc498acdce671431
6P_kimdongmin <6P_kimdongmin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-06-27 06:34:23 +0000

    [Merge] Develop -> CircleLogicPuzzle
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@650 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/CircleLogicPuzzle/Source/The_Phantom_Twins/OutGame/MainMenu/W_MainMenu.cpp
+++ Project/Feature/CircleLogicPuzzle/Source/The_Phantom_Twins/OutGame/MainMenu/W_MainMenu.cpp
@@ -16,11 +16,6 @@ void UW_MainMenu::NativeConstruct()
 
 void UW_MainMenu::StartLobby(const FName LevelName)
 {
-    FString IP = UIPEncryption::GetLocalIPAddress();
-    FString HexString = UIPEncryption::IP2Code(IP);
-    UE_LOG(LogTemp, Error, TEXT("Sever: %s"), *HexString);
-
-
 	UGameplayStatics::OpenLevel(GetWorld(), LevelName, true, TEXT("listen"));
 }
 
@@ -30,8 +25,6 @@ void UW_MainMenu::ConnectLobby(const FName LevelName)
     {
         FString Code = LevelName.ToString();
         FString IP = UIPEncryption::Code2IP(Code);
-
-        UE_LOG(LogTemp, Error, TEXT("Client: %s"), *IP);
 
         PC->ClientTravel(IP, ETravelType::TRAVEL_Absolute);
     }
