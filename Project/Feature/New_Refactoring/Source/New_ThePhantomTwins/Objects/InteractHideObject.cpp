77598dd50ea7e2e58046530e466b378dbd1d1c2a
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-07-07 08:31:27 +0000

    commit test
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@682 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/New_Refactoring/Source/New_ThePhantomTwins/Objects/InteractHideObject.cpp
@@ -0,0 +1,51 @@
+﻿
+#include "InteractHideObject.h"
+#include "Net/UnrealNetwork.h"
+#include "Camera/CameraComponent.h"
+
+
+AInteractHideObject::AInteractHideObject() : ABaseObject()
+{
+	MeshComp->SetCollisionProfileName(TEXT("BlockAll"));
+
+	// Camera
+	HideCameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("BideCamComponent"));
+	HideCameraComp->SetupAttachment(RootSceneComp);
+
+}
+
+void AInteractHideObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AInteractHideObject::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//DOREPLIFETIME(AInteractHideObject, );
+}
+
+void AInteractHideObject::OnInteractServer_Implementation(const APawn* Interactor)
+{
+	UE_LOG(LogTemp, Warning, TEXT("InteractHideObject::OnInteractServer"));
+
+	// 플레이어가 들어있다는거 체크
+}
+
+void AInteractHideObject::OnInteractClient_Implementation(const APawn* Interactor)
+{
+	// 플레이어 카메라를 object 카메라로 돌리기
+}
+
+bool AInteractHideObject::CanInteract_Implementation(const APawn* Interactor, bool bIsDetected)
+{
+	UE_LOG(LogTemp, Warning, TEXT("InteractHideObject::CanInteract"));
+
+	SetWidgetVisible(bIsDetected);
+
+	bCanInteract = bIsDetected;
+
+	return bCanInteract;
+}
