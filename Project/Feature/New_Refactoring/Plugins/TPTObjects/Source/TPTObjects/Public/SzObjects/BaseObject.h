db84eb08140f0f675b426e2df9f553768dec1218
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-07-07 04:08:34 +0000

    [New_Refactoring] object plugin 추가
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@672 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/New_Refactoring/Plugins/TPTObjects/Source/TPTObjects/Public/SzObjects/BaseObject.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseObject.generated.h"
+
+UCLASS()
+class TPTOBJECTS_API ABaseObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ABaseObject();
+
+protected:
+	virtual void BeginPlay() override;
+
+	void SetWidgetVisible(bool Visible);
+
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject | Components")
+	TObjectPtr<class USceneComponent> RootSceneComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject | Components")
+	TObjectPtr<class UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject | Components")
+	TObjectPtr<class USphereComponent> SphereCollisionComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject | Components")
+	TObjectPtr<class UOutlineComponent> OutlineComp;
+
+
+	// 가까운 오브젝트 확인용 위젯
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject | NearWidget")
+	TObjectPtr<class UWidgetComponent> NearWidgetComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject | NearWidget")
+	TSubclassOf<class UUserWidget> NearWidgetClass;
+	
+    // AI percrption(적이 사용)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject | AI")
+    TObjectPtr<class UAIPerceptionStimuliSourceComponent> StimuliSource;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseObject")
+	bool bCanInteract = true;
+
+};
---
+++ Project/Feature/New_Refactoring/Plugins/TPTObjects/Source/TPTObjects/Public/SzObjects/ObjectState.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ObjectState.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FItemState
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsPickedUp = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bDestory = false;
+};
+
+
+USTRUCT(BlueprintType)
+struct FHideState
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hide")
+	bool bHasPlayer = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hide")
+	int32 PlayerNum = 0;
+};
