5e8b0357f10653809db7c26d708cc1b340648fb4
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-07-15 04:53:22 +0000

    [New_Refactoring] - HUD 만드는 중. HUD에 들어가는 위젯들 만들었다. HP바는 K 누르면 변경됨.
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@758 323376ff-746e-4b47-8525-04d7babb2b8c

---
+++ Project/Feature/New_Refactoring/Source/New_ThePhantomTwins/UI/HUD/HUD_PhantomTwins.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD_PhantomTwins.h"
+#include "PlayerStatusWidget.h"
+#include "Blueprint/UserWidget.h"
+
+void AHUD_PhantomTwins::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (PlayerStatusWidgetClass)
+    {
+        PlayerStatusWidget = CreateWidget<UPlayerStatusWidget>(GetWorld(), PlayerStatusWidgetClass);
+        if (PlayerStatusWidget)
+        {
+            PlayerStatusWidget->AddToViewport();
+        }
+    }
+}
+
+void AHUD_PhantomTwins::UpdateHP(float HP)
+{
+    if (PlayerStatusWidget)
+    {
+        PlayerStatusWidget->SetHP(HP); // UHPBarWidget이 실제 UI에 반영
+    }
+}
