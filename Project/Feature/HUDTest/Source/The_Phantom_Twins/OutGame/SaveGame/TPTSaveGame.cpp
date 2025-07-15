7f84f02703461e2631884cb2cdcbb009a260315b
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-07-15 06:47:16 +0000

    Remove HUDTest
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@764 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/HUDTest/Source/The_Phantom_Twins/OutGame/SaveGame/TPTSaveGame.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "TPTSaveGame.h"
-#include "Kismet/GameplayStatics.h"
-
-UTPTSaveGame::UTPTSaveGame()
-{
-}
