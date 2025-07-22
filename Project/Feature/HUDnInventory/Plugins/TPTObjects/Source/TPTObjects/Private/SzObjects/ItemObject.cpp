c0f95013995a04bcfe4568821e4a5091922b94b4
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-07-22 07:56:45 +0000

    [HUDnInventory] - ItemObject에 GAS 적용. :: GE 사용하여 interactor에게 Damage 줌. GC 사용하여 interact하면 폭발 이펙트 출력 - ItemPickUP GC BP랑 Damage GE BP 추가.
    아직 client에 버그 있음. GC는 잘 되는데 GE는 안됨. host가 interact 했을 때 guest한테 오브젝트 삭제가 바로 안되는 버그도 있음
    
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@879 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/HUDnInventory/Plugins/TPTObjects/Source/TPTObjects/Private/SzObjects/ItemObject.cpp
+++ Project/Feature/HUDnInventory/Plugins/TPTObjects/Source/TPTObjects/Private/SzObjects/ItemObject.cpp
@@ -1,8 +1,9 @@
 ﻿
 #include "SzObjects/ItemObject.h"
 #include "Net/UnrealNetwork.h"
-#include "AbilitySystemComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
 AItemObject::AItemObject() : AInteractableObject()
 {
@@ -30,24 +31,33 @@ void AItemObject::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifet
 	//DOREPLIFETIME(AItemObject, );
 }
 
+void AItemObject::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	ASC->InitAbilityActorInfo(this, this);
+}
+
 void AItemObject::OnInteractServer_Implementation(const APawn* Interactor)
 {
-	UseItemEffectServer();
+	UseItemEffectServer(Interactor);
 }
 
 void AItemObject::OnInteractClient_Implementation(const APawn* Interactor)
 {
 	AInteractableObject::OnInteractClient_Implementation(Interactor);
 	//UE_LOG(LogTemp, Log, TEXT("AItemObject::OnInteractClient"));
-	UseItemEffectClient();
+	UseItemEffectClient(Interactor);
 }
 
-void AItemObject::UseItemEffectServer_Implementation()
+void AItemObject::UseItemEffectServer_Implementation(const APawn* Interactor)
 {
-	//UE_LOG(LogTemp, Log, TEXT("AItemObject::UseItemEffectServer"));
+	// player가 아이템 먹었을 때 수행할 로직 구현
+	InvokeGameplayCue(Interactor);		// 자기 자신 이펙트 재생
+	ApplyEffectToTarget(Interactor);		// 상대방한테 게임플레이 이펙트 발동 시킴
 }
 
-void AItemObject::UseItemEffectClient_Implementation()
+void AItemObject::UseItemEffectClient_Implementation(const APawn* Interactor)
 {
 }
 
@@ -62,4 +72,39 @@ void AItemObject::DestroyItem_Implementation()
 
 	SetActorEnableCollision(false);	// 더이상 이벤트가 일어나지 않도록 false
 	SetLifeSpan(2.0f);				// 2초뒤에 자동으로 사라지도록.
+}
+
+void AItemObject::ApplyEffectToTarget(const APawn* Interactor)
+{	// 상대방에게 영향을 끼치는 기능
+	
+	AActor* TargetActor = const_cast<APawn*>(Interactor);
+
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+
+	if (TargetASC)
+	{
+		// 영향을 끼치기 위해서(게임플레이 이펙트를 보내기 위해서) 먼저 이펙트 컨텍스트를 제작해야함
+		FGameplayEffectContextHandle EffectContext = TargetASC->MakeEffectContext();
+		EffectContext.AddSourceObject(this); // 근데 오브젝트가 바로 사라져서 큰 의미 없음
+
+		// 밖으로 내보낼 게임플레이 이펙트의 스펙을 만듦 => 이를 위해선 어떤 게임플레이 이펙트를 만들지 클래스를 지정해야 함
+		FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1, EffectContext);
+		if (EffectSpecHandle.IsValid())
+		{
+			// BP_TargetASC에서 발동하는 것이기 때문에 ...Self 함수 사용
+			TargetASC->BP_ApplyGameplayEffectSpecToSelf(EffectSpecHandle);
+		}
+	}
+}
+
+void AItemObject::InvokeGameplayCue(const APawn* Interactor)
+{  // 자기 자신에게 특수효과를 재생
+
+	AActor* TargetActor = const_cast<APawn*>(Interactor);
+
+	FGameplayCueParameters Param;
+	Param.SourceObject = this;
+	Param.Instigator = TargetActor;
+	Param.Location = GetActorLocation();
+	ASC->ExecuteGameplayCue(GameplayCueTag, Param);	// 게임플레이 큐 용 태그를 설정할 땐, 헤더에서 메타 정보를 넣으면 좋음
 }
--- Project/Feature/HUDnInventory/Plugins/TPTObjects/Source/TPTObjects/Public/SzObjects/ItemObject.h
+++ Project/Feature/HUDnInventory/Plugins/TPTObjects/Source/TPTObjects/Public/SzObjects/ItemObject.h
@@ -4,8 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "SzObjects/InteractableObject.h"
-//#include "SzInterface/Interact.h"
 #include "AbilitySystemInterface.h"
+#include "GameplayTagContainer.h"
 #include "ItemObject.generated.h"
 
 UCLASS()
@@ -22,26 +22,36 @@ protected:
 public:
 	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PostInitializeComponents() override;
 
     virtual void OnInteractServer_Implementation(const APawn* Interactor) override;
     virtual void OnInteractClient_Implementation(const APawn* Interactor) override;
 
     // 자식클래스 혹은 블루프린트에서 동작을 구현할 수 있도록 선언(서버용)
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "ItemObject")
-	void UseItemEffectServer();
-	virtual void UseItemEffectServer_Implementation();
+	void UseItemEffectServer(const APawn* Interactor);
+	virtual void UseItemEffectServer_Implementation(const APawn* Interactor);
 
     // (Client용)
     UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "ItemObject")
-	void UseItemEffectClient();
-	virtual void UseItemEffectClient_Implementation();
+	void UseItemEffectClient(const APawn* Interactor);
+	virtual void UseItemEffectClient_Implementation(const APawn* Interactor);
 
     UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "ItemObject")
 	void DestroyItem();
 	virtual void DestroyItem_Implementation();
 
 
+	void ApplyEffectToTarget(const APawn* Interactor);	// 아이템을 먹었을 때 먹은 대상에게 게임플레이 이펙트를 적용시켜주는 함수
+	void InvokeGameplayCue(const APawn* Interactor);		// 아이템에 대한 자체 이펙트(상자 오픈 이펙트)를 재생하기 위한 함수
+
 protected:
 	UPROPERTY()
 	TObjectPtr<class UAbilitySystemComponent> ASC;
+
+	UPROPERTY(EditAnywhere, Category = GAS)
+	TSubclassOf<class UGameplayEffect> GameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta=(Categories=GameplayCue))
+	FGameplayTag GameplayCueTag;
 };
