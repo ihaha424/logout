ac970201d95dc04065f91ae46ab3cfac03108a53
6P_hamshina <6P_hamshina@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-07-24 10:56:51 +0000

    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@920 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/Gas_Player/Source/New_ThePhantomTwins/GA/Action/GA_UseItemSlot.cpp
+++ Project/Feature/Gas_Player/Source/New_ThePhantomTwins/GA/Action/GA_UseItemSlot.cpp
@@ -9,18 +9,18 @@
 UGA_UseItemSlot::UGA_UseItemSlot()
 {
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
-	//TPT_LOG(GALog, Error, TEXT(""));
 }
 
-void UGA_UseItemSlot::ActivateAbility(const FGameplayAbilitySpecHandle Handle,const FGameplayAbilityActorInfo* ActorInfo,const FGameplayAbilityActivationInfo ActivationInfo,const FGameplayEventData* TriggerEventData)
+void UGA_UseItemSlot::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
-	TPT_LOG(GALog, Warning, TEXT(" "));
     Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+    NULLCHECK_RETURN_LOG(TriggerEventData, GALog, Error, );
 
-    if (TriggerEventData)
-    {
-        float SlotNumber = TriggerEventData->EventMagnitude;
-        TPT_LOG(GALog, Warning, TEXT(" %f"), SlotNumber);
-        // 이 값을 이용해 아이템 사용 등 원하는 행동 수행
-    }
+    float SlotNumber = TriggerEventData->EventMagnitude;
+
+
+    //TPT_LOG(GALog, Warning, TEXT(" %f"), SlotNumber);
+    // 이 값을 이용해 아이템 사용 등 원하는 행동 수행
+ 
+    EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
