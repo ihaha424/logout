5af0fa5d405e8a7989d48456ca7f851cd052d993
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-07-24 12:41:57 +0000

    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@931 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/HUDnInventory/Source/New_ThePhantomTwins/UI/HUD/HUD_PhantomTwins.cpp
+++ Project/Feature/HUDnInventory/Source/New_ThePhantomTwins/UI/HUD/HUD_PhantomTwins.cpp
@@ -1,5 +1,7 @@
 ﻿
 #include "HUD_PhantomTwins.h"
+
+#include "AbilitySystemComponent.h"
 #include "Blueprint/UserWidget.h"
 
 #include "PlayerStatusWidget.h"
@@ -8,6 +10,10 @@
 #include "PlayerSkillWidget.h"
 #include "InventoryWidget.h"
 #include "../../Log/TPTLog.h"
+#include "Attribute/PlayerAttributeSet.h"
+#include "GameFramework/PlayerState.h"
+#include "Player/PC_Player.h"
+#include "Player/PlayerCharacter.h"
 
 
 void AHUD_PhantomTwins::BeginPlay()
@@ -68,9 +74,73 @@ void AHUD_PhantomTwins::BeginPlay()
             InventoryWidget->LoadInventory(nullptr);       // 테스트용 임시 코드
         }
     }
+
+ //   ASC->GetGameplayAttributeValueChangeDelegate(UPlayerAttributeSet::GetHPAttribute())
+	//.AddUObject(this, &AHUD_PhantomTwins::UpdateHP);
+ //   ASC->GetGameplayAttributeValueChangeDelegate(UPlayerAttributeSet::GetMentalPointAttribute())
+ //       .AddUObject(this, &AHUD_PhantomTwins::UpdateMental);
+    //ASC->GetGameplayAttributeValueChangeDelegate(AttSet->GetStaminaAttribute())
+    //    .AddUObject(this, &AHUD_PhantomTwins::UpdateStamina);
+
+    /*FDelegateHandle TEMP = ASC->GetGameplayAttributeValueChangeDelegate(AttSet->GetCoreEnergyAttribute())
+        .AddUObject(this, &AHUD_PhantomTwins::UpdateCoreEnergy);*/
+    //TPT_LOG(HUDLog, Log, TEXT("CoreEnergy Delegate 바인딩 실행됨. %d"), TEMP.IsValid());
 }
 
+void AHUD_PhantomTwins::TryInitHUD()
+{
+    APC_Player* PC = Cast<APC_Player>(GetOwningPlayerController());
+    if (!PC)
+    {
+        ScheduleNextInit(); return;
+    }
 
+    APlayerCharacter* Pawn = Cast<APlayerCharacter>(PC->GetPawn());
+    if (!Pawn)
+    {
+        ScheduleNextInit(); return;
+    }
+
+    APlayerState* PS = Pawn->GetPlayerState();
+    if (!PS)
+    {
+        ScheduleNextInit(); return;
+    }
+
+    UAbilitySystemComponent* ASC = PS->FindComponentByClass<UAbilitySystemComponent>();
+    if (!ASC)
+    {
+        ScheduleNextInit(); return;
+    }
+
+    const UPlayerAttributeSet* AttSet = ASC->GetSet<UPlayerAttributeSet>();
+    if (!AttSet)
+    {
+        ScheduleNextInit(); return;
+    }
+
+    // 모든 것이 준비 완료. 한 번만 실행!
+    InitHUD_Internal(PC, Pawn, PS, ASC, AttSet);
+    bIsHUDInitDone = true; // 멤버 변수로 만들 것
+}
+void AHUD_PhantomTwins::ScheduleNextInit()
+{
+    if (!bIsHUDInitDone)
+    {
+        // 0.05초 후에 다시 Init 시도 (틱마다 말고, 부담 적게)
+        GetWorld()->GetTimerManager().SetTimerForNextTick(this, &AHUD_PhantomTwins::TryInitHUD);
+    }
+}
+
+void AHUD_PhantomTwins::InitHUD_Internal(APC_Player* PC, APlayerCharacter* Pawn, APlayerState* PS, UAbilitySystemComponent* ASC, const UPlayerAttributeSet* AttSet)
+{
+    // HUD 값들 세팅
+    UpdateHP(AttSet->GetHP());
+    UpdateMental(AttSet->GetMentalPoint());
+    UpdateStamina(AttSet->GetStamina());
+    UpdateCoreEnergy(AttSet->GetCoreEnergy());
+    // 기타 위젯 바인딩, 이벤트 바인드 등등
+}
 /* PlayerStatus */
 void AHUD_PhantomTwins::UpdateHP(int32 HP)
 {
@@ -132,10 +202,10 @@ void AHUD_PhantomTwins::SetPassiveSkillIcon(UTexture2D* PassiveSkillIcon)
     }
 }
 
-void AHUD_PhantomTwins::UpdateSkillPoints(int32 SkillPointsNum)
+void AHUD_PhantomTwins::UpdateCoreEnergy(const int32 val)
 {
     if (PlayerSkillWidget)
     {
-        PlayerSkillWidget->ShowSkillPoints(SkillPointsNum);
+        PlayerSkillWidget->ShowCoreEnergy(val);
     }
 }
