1fc8bb095fc702a80fa602ba05a5f3aa03a78b10
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-08-06 09:22:46 +0000

    [merge] ConsoleObject->Develop merge
    - console 오브젝트 만듦. 근데 버그 있음
    
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@1256 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/Objects/ConsoleObject.h
+++ Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/Objects/ConsoleObject.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -25,17 +25,45 @@ public:
 
 	virtual bool CanInteract_Implementation(const APawn* Interactor, bool bIsDetected) override;
 	virtual void OnInteractServer_Implementation(const APawn* Interactor) override;
-	virtual void OnInteractClient_Implementation(const APawn* Interactor) override;
 
 	virtual void SetWidgetVisible(bool bVisible) override;
 
+	UFUNCTION()
+    void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+    UFUNCTION()
+    void OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+	bool AreAllTriggerActived() const;
+
 	virtual void OnRep_bIsActived() override;
 
-public:
+protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ConsoleObject | ObjectWidget")
 	TObjectPtr<class UWidgetComponent> LockWidgetComp;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ConsoleObject | ObjectWidget")
 	TSubclassOf<class UUserWidget> LockWidgetClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ConsoleObject | Hide")
+	TObjectPtr<class USphereComponent> Trigger;
+
+	UPROPERTY(Replicated)
+	int32 HasPlayerNum = 0;
+
+	UPROPERTY(EditAnywhere, Category = "ConsoleObject")
+	int32 MaxPlayerNum = 2;
+
+	// 문이 열리기 위해 필요한 Actor 목록
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ConsoleObject")
+	TArray<AActor*> RequiredList;
+
+	// 필요 활성화 수 (0이면 RequiredList의 전체 수가 기본값)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ConsoleObject")
+    int32 MinRequiredCount = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ConsoleObject | Door")
+	TObjectPtr<class ADoor> ConnectedDoor;
 
 };
