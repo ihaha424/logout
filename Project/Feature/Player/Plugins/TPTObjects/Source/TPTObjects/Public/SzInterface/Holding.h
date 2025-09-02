702f77c55980eed831c684e98d64ad0bf4343731
6P_kimkaeun <6P_kimkaeun@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-02 02:53:59 +0000

    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@1505 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/Player/Plugins/TPTObjects/Source/TPTObjects/Public/SzInterface/Holding.h
+++ Project/Feature/Player/Plugins/TPTObjects/Source/TPTObjects/Public/SzInterface/Holding.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
-#include "Interact.generated.h"
+#include "Holding.generated.h"
 
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
