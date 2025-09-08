03aa8099bc86e640bf4cc2f87cf63ea75fbeee11
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-08 04:29:28 +0000

    [merge] GA_Key 코드 추가.
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@1729 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/GA/Object/GA_Key.h
+++ Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/GA/Object/GA_Key.h
@@ -1,14 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
 #include "GA_Key.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class NEW_THEPHANTOMTWINS_API UGA_Key : public UGameplayAbility
 {
@@ -17,11 +13,10 @@ class NEW_THEPHANTOMTWINS_API UGA_Key : public UGameplayAbility
 public:
 	UGA_Key();
 
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
-
-	UFUNCTION()
-	void OnMontageComplete();
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
-	TObjectPtr<UAnimMontage> UseKeyMontage;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+private:
+	TObjectPtr<class APlayerCharacter> Character = nullptr;
+	TObjectPtr<AActor> TargetActor = nullptr;
 };
