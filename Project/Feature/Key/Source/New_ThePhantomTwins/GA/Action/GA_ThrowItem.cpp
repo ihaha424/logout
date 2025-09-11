82fb3d53c98d7ab9610adac040ffc9b4a14fb700
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-11 08:16:56 +0000

    [Key] - BP_ThrowEMP guest가 보이는지 테스트 해봐야함
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@1906 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/Key/Source/New_ThePhantomTwins/GA/Action/GA_ThrowItem.cpp
+++ Project/Feature/Key/Source/New_ThePhantomTwins/GA/Action/GA_ThrowItem.cpp
@@ -1,7 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "GA_ThrowItem.h"
+﻿#include "GA_ThrowItem.h"
 #include "Objects/ThrowNoiseBomb.h"
 #include "Objects/ThrowEMP.h"
 #include "Kismet/KismetMathLibrary.h"
@@ -22,193 +19,168 @@ void UGA_ThrowItem::ActivateAbility(const FGameplayAbilitySpecHandle Handle, con
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-    if (HasAuthority(&ActivationInfo))
-    {
-        EItemType ItemType = EItemType::None;
+	if (HasAuthority(&ActivationInfo))
+	{
+		EItemType ItemType = EItemType::None;
+		if (TriggerEventData)
+		{
+			ItemType = static_cast<EItemType>((int32)TriggerEventData->EventMagnitude);
+		}
 
-        // TriggerEventData에서 ItemType 추출
-        if (TriggerEventData)
-        {
-            ItemType = static_cast<EItemType>((int32)TriggerEventData->EventMagnitude);
-        }
+		SpawnThrowableItem(ItemType);
+	}
 
-        SpawnThrowableItem(ItemType);
-    }
-
-    EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
 
 void UGA_ThrowItem::SpawnThrowableItem(EItemType ItemType)
 {
-    if (!NoiseBombClass)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("NoiseBombClass is not set!"));
-        return;
-    }
+	if (!NoiseBombClass || !EMPClass)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Required classes (NoiseBombClass or EMPClass) are not set!"));
+		return;
+	}
 
-    AActor* OwnerActor = GetOwningActorFromActorInfo();
-    if (!OwnerActor)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("UGA_NoiseBomb: No owning actor."));
-        return;
-    }
+	AActor* OwnerActor = GetOwningActorFromActorInfo();
+	if (!OwnerActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UGA_ThrowItem: No owning actor."));
+		return;
+	}
 
-    // 1) 스폰 시작 위치: RightHandSocket (있으면)
-    FVector SpawnLocation = GetRightHandSocketLocation();
-    if (SpawnLocation.IsZero())
-    {
-        // 실패 시 액터 위치를 사용
-        SpawnLocation = OwnerActor->GetActorLocation();
-    }
+	FVector SpawnLocation = GetRightHandSocketLocation();
+	if (SpawnLocation.IsZero())
+	{
+		SpawnLocation = OwnerActor->GetActorLocation();
+	}
 
-    // 2) 목표 위치 계산 (시야/컨트롤러 방향 우선, 없으면 Actor Forward)
-    FVector TargetLocation = CalculateTargetLocation(SpawnLocation);
+	FVector TargetLocation = CalculateTargetLocation(SpawnLocation);
+	FRotator SpawnRotation = GetThrowRotation(SpawnLocation, TargetLocation);
 
-    // 3) 스폰 회전 (LookAt)
-    FRotator SpawnRotation = GetThrowRotation(SpawnLocation, TargetLocation);
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = OwnerActor;
+	SpawnParams.Instigator = Cast<APawn>(OwnerActor);
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
-    // 4) 스폰 파라미터
-    FActorSpawnParameters SpawnParams;
-    SpawnParams.Owner = OwnerActor;
-    SpawnParams.Instigator = Cast<APawn>(OwnerActor);
-    SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	UWorld* World = GetWorld();
+	if (!World) return;
 
-    UWorld* World = GetWorld();
-    if (!World) return;
+	FVector LaunchVelocity;
+	bool bHaveVelocity = UGameplayStatics::SuggestProjectileVelocity_CustomArc(
+		World,
+		LaunchVelocity,
+		SpawnLocation,
+		TargetLocation,
+		0.0f,
+		Arc
+	);
 
+	auto SetupProjectile = [&](AActor* ThrowActor)
+		{
+			if (!ThrowActor) return false;
 
-    // 5) Launch Velocity 계산 (포물선 궤적)
-    FVector LaunchVelocity;
-    bool bHaveVelocity = UGameplayStatics::SuggestProjectileVelocity_CustomArc(
-        World,
-        LaunchVelocity,
-        SpawnLocation,
-        TargetLocation,
-        0.0f,     // Override gravity Z (0 -> use world gravity)
-        Arc      // Arc parameter
-    );
+			TPT_LOG(GALog, Warning, TEXT("UGA_ThrowItem :: %s 생성"), *ThrowActor->GetName());
 
-    // 6) 액터 스폰
-    switch (ItemType)
-    {
-    case EItemType::NoiseBomb:
-    {
-        AThrowNoiseBomb* ThrowActor = World->SpawnActor<AThrowNoiseBomb>(NoiseBombClass, SpawnLocation, SpawnRotation, SpawnParams);
+			UProjectileMovementComponent* ProjectileMovementComponent = nullptr;
 
-        if (!ThrowActor)
-        {
-            UE_LOG(LogTemp, Warning, TEXT("Failed to spawn ThrowActor."));
-            return;
-        }
+			if (ItemType == EItemType::NoiseBomb)
+			{
+				AThrowNoiseBomb* NoiseBomb = Cast<AThrowNoiseBomb>(ThrowActor);
+				ProjectileMovementComponent = NoiseBomb ? NoiseBomb->ProjectileMovementComponent : nullptr;
+			}
+			else if (ItemType == EItemType::EMP)
+			{
+				AThrowEMP* EMP = Cast<AThrowEMP>(ThrowActor);
+				ProjectileMovementComponent = EMP ? EMP->ProjectileMovementComponent : nullptr;
+			}
 
-        TPT_LOG(GALog, Warning, TEXT("UGA_ThrowItem :: NoiseBomb 생성"));
+			if (ProjectileMovementComponent)
+			{
+				InitializeProjectileMovement(ProjectileMovementComponent, bHaveVelocity ? LaunchVelocity : SpawnRotation.Vector() * ProjectileMovementComponent->InitialSpeed, SpawnRotation);
+				return true;
+			}
 
-        if (bHaveVelocity && ThrowActor->ProjectileMovementComponent)
-        {
-            // ProjectileMovementComponent에 속도 적용
-            ThrowActor->ProjectileMovementComponent->Velocity = LaunchVelocity;
-            ThrowActor->ProjectileMovementComponent->SetActive(true);
-            ThrowActor->ProjectileMovementComponent->Activate(true);
-            ThrowActor->ProjectileMovementComponent->UpdateComponentVelocity();
-        }
-        else if (ThrowActor->ProjectileMovementComponent)
-        {
-            // 실패하면 그냥 앞으로 일정 속도 부여
-            FVector Fwd = SpawnRotation.Vector();
-            ThrowActor->ProjectileMovementComponent->Velocity = Fwd * ThrowActor->ProjectileMovementComponent->InitialSpeed;
-            ThrowActor->ProjectileMovementComponent->SetActive(true);
-            ThrowActor->ProjectileMovementComponent->Activate(true);
-            ThrowActor->ProjectileMovementComponent->UpdateComponentVelocity();
-        }
+			return false;
+		};
 
-        break;
-    }
-    case EItemType::EMP:
-    {
-        AThrowEMP* ThrowActor = World->SpawnActor<AThrowEMP>(EMPClass, SpawnLocation, SpawnRotation, SpawnParams);
-
-        if (!ThrowActor)
-        {
-            TPT_LOG(GALog, Warning, TEXT("Failed to spawn ThrowActor."));
-            return;
-        }
-
-        TPT_LOG(GALog, Warning, TEXT("UGA_ThrowItem :: ThrowEMP 생성"));
-
-        if (bHaveVelocity && ThrowActor->ProjectileMovementComponent)
-        {
-            // ProjectileMovementComponent에 속도 적용
-            ThrowActor->ProjectileMovementComponent->Velocity = LaunchVelocity;
-            ThrowActor->ProjectileMovementComponent->SetActive(true);
-            ThrowActor->ProjectileMovementComponent->Activate(true);
-            ThrowActor->ProjectileMovementComponent->UpdateComponentVelocity();
-        }
-        else if (ThrowActor->ProjectileMovementComponent)
-        {
-            // 실패하면 그냥 앞으로 일정 속도 부여
-            FVector Fwd = SpawnRotation.Vector();
-            ThrowActor->ProjectileMovementComponent->Velocity = Fwd * ThrowActor->ProjectileMovementComponent->InitialSpeed;
-            ThrowActor->ProjectileMovementComponent->SetActive(true);
-            ThrowActor->ProjectileMovementComponent->Activate(true);
-            ThrowActor->ProjectileMovementComponent->UpdateComponentVelocity();
-        }
-
-        break;
-    }
-
-    default:
-        break;
-    }
+	switch (ItemType)
+	{
+	case EItemType::NoiseBomb:
+	{
+		AThrowNoiseBomb* ThrowActor = World->SpawnActor<AThrowNoiseBomb>(NoiseBombClass, SpawnLocation, SpawnRotation, SpawnParams);
+		if (!SetupProjectile(ThrowActor))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to spawn or setup NoiseBomb actor."));
+		}
+		break;
+	}
+	case EItemType::EMP:
+	{
+		AThrowEMP* ThrowActor = World->SpawnActor<AThrowEMP>(EMPClass, SpawnLocation, SpawnRotation, SpawnParams);
+		if (!SetupProjectile(ThrowActor))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to spawn or setup EMP actor."));
+		}
+		break;
+	}
+	default:
+		break;
+	}
 }
 
-FVector UGA_ThrowItem::CalculateTargetLocation(const FVector& StartLocation)
+FVector UGA_ThrowItem::CalculateTargetLocation(const FVector& StartLocation) const
 {
-    AActor* OwnerActor = GetOwningActorFromActorInfo();
-    if (!OwnerActor)
-        return StartLocation + FVector::ForwardVector * ThrowDistance;
+	AActor* OwnerActor = GetOwningActorFromActorInfo();
+	if (!OwnerActor)
+		return StartLocation + FVector::ForwardVector * ThrowDistance;
 
-    // 우선적으로 컨트롤러의 뷰 방향(카메라)을 사용
-    FVector ViewDir = FVector::ZeroVector;
-    APawn* Pawn = Cast<APawn>(OwnerActor);
-    if (Pawn && Pawn->GetController())
-    {
-        FRotator ControlRot = Pawn->GetController()->GetControlRotation();
-        ViewDir = ControlRot.Vector();
-    }
+	FVector ViewDir = FVector::ZeroVector;
+	APawn* Pawn = Cast<APawn>(OwnerActor);
+	if (Pawn && Pawn->GetController())
+	{
+		FRotator ControlRot = Pawn->GetController()->GetControlRotation();
+		ViewDir = ControlRot.Vector();
+	}
 
-    if (ViewDir.IsNearlyZero())
-    {
-        // 없으면 액터 전방 사용
-        ViewDir = OwnerActor->GetActorForwardVector();
-    }
+	if (ViewDir.IsNearlyZero())
+	{
+		ViewDir = OwnerActor->GetActorForwardVector();
+	}
 
-    return StartLocation + ViewDir.GetSafeNormal() * ThrowDistance;
+	return StartLocation + ViewDir.GetSafeNormal() * ThrowDistance;
 }
 
 FVector UGA_ThrowItem::GetRightHandSocketLocation() const
 {
-    AActor* OwnerAvatar = GetAvatarActorFromActorInfo();
-    if (!OwnerAvatar) return FVector::ZeroVector;
+	AActor* OwnerAvatar = GetAvatarActorFromActorInfo();
+	if (!OwnerAvatar) return FVector::ZeroVector;
 
-    // 만약 ACharacter라면 SkeletalMesh의 소켓 위치를 사용
-    const APlayerCharacter* PlayerChar = Cast<APlayerCharacter>(OwnerAvatar);
+	const APlayerCharacter* PlayerChar = Cast<APlayerCharacter>(OwnerAvatar);
+	if (PlayerChar)
+	{
+		if (const USkeletalMeshComponent* MeshComp = PlayerChar->GetMesh())
+		{
+			if (MeshComp->DoesSocketExist(TEXT("RightHandSocket")))
+			{
+				return MeshComp->GetSocketLocation(TEXT("RightHandSocket"));
+			}
+		}
+	}
 
-    if (PlayerChar)
-    {
-        if (const USkeletalMeshComponent* MeshComp = PlayerChar->GetMesh())
-        {
-            if (MeshComp->DoesSocketExist(TEXT("RightHandSocket")))
-            {
-                return MeshComp->GetSocketLocation(TEXT("RightHandSocket"));
-            }
-        }
-    }
-
-    // 소켓이 없으면 액터 위치 반환 (caller에서 예외 처리)
-    return FVector::ZeroVector;
+	return FVector::ZeroVector;
 }
 
 FRotator UGA_ThrowItem::GetThrowRotation(const FVector& StartLocation, const FVector& TargetLocation) const
 {
-    return UKismetMathLibrary::FindLookAtRotation(StartLocation, TargetLocation);
+	return UKismetMathLibrary::FindLookAtRotation(StartLocation, TargetLocation);
+}
+
+void UGA_ThrowItem::InitializeProjectileMovement(UProjectileMovementComponent* ProjectileMovementComponent, const FVector& LaunchVelocity, const FRotator& SpawnRotation) const
+{
+	if (!ProjectileMovementComponent) return;
+
+	ProjectileMovementComponent->Velocity = LaunchVelocity;
+	ProjectileMovementComponent->SetActive(true);
+	ProjectileMovementComponent->Activate(true);
+	ProjectileMovementComponent->UpdateComponentVelocity();
 }
--- Project/Feature/Key/Source/New_ThePhantomTwins/GA/Action/GA_ThrowItem.h
+++ Project/Feature/Key/Source/New_ThePhantomTwins/GA/Action/GA_ThrowItem.h
@@ -1,44 +1,38 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
 #include "Objects/ItemData.h"
 #include "GA_ThrowItem.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class NEW_THEPHANTOMTWINS_API UGA_ThrowItem : public UGameplayAbility
 {
 	GENERATED_BODY()
+
 public:
 	UGA_ThrowItem();
 
 protected:
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
-    // 소음폭탄 액터 클래스
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Noise")
-    TSubclassOf<class AThrowNoiseBomb> NoiseBombClass;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "ThrowItem")
+	TSubclassOf<class AThrowNoiseBomb> NoiseBombClass;
 
-    // EMP 액터 클래스
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Noise")
-    TSubclassOf<class AThrowEMP> EMPClass;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "ThrowItem")
+	TSubclassOf<class AThrowEMP> EMPClass;
 
-    // 투척 거리 (목표까지의 거리)
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Noise")
-    float ThrowDistance = 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ThrowItem")
+	float ThrowDistance = 1000.0f;
 
-    // 궤적 아크 값 (0.0 - 1.0, 값이 클수록 높게 던짐)
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Noise")
-    float Arc = 0.5f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "ThrowItem")
+	float Arc = 0.5f;
 
 private:
-    void SpawnThrowableItem(EItemType ItemType);
-    FVector CalculateTargetLocation(const FVector& StartLocation);
-    FVector GetRightHandSocketLocation() const;
-    FRotator GetThrowRotation(const FVector& StartLocation, const FVector& TargetLocation) const;
+	void SpawnThrowableItem(EItemType ItemType);
+	FVector CalculateTargetLocation(const FVector& StartLocation) const;
+	FVector GetRightHandSocketLocation() const;
+	FRotator GetThrowRotation(const FVector& StartLocation, const FVector& TargetLocation) const;
+
+	void InitializeProjectileMovement(class UProjectileMovementComponent* ProjectileMovementComponent, const FVector& LaunchVelocity, const FRotator& SpawnRotation) const;
 };
