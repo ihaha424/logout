1de826a7ba614936b757ca74f66ee7d633ff5f65
6P_kimkaeun <6P_kimkaeun@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-11 02:45:23 +0000

    포물선 작업중
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@1872 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/LogOutPlayer/Source/New_ThePhantomTwins/GA/Action/GA_AimItem.cpp
+++ Project/Feature/LogOutPlayer/Source/New_ThePhantomTwins/GA/Action/GA_AimItem.cpp
@@ -6,6 +6,9 @@
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "Log/TPTLog.h"
 #include "Tags/TPTGameplayTags.h"
+#include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 UGA_AimItem::UGA_AimItem()
 {
@@ -21,4 +24,54 @@ void UGA_AimItem::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 	PlayHoldingItemMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("HoldingItemMontage"), HoldingItemMontage, 1.0f);
 	PlayHoldingItemMontageTask->ReadyForActivation();
+
+	//
+	//if (AActor* Avatar = GetAvatarActorFromActorInfo())
+	//{
+	//	AimDecal = NewObject<UDecalComponent>(Avatar);
+	//	AimDecal->RegisterComponent();
+	//	AimDecal->SetDecalMaterial(DecalMaterial);
+	//	AimDecal->DecalSize = FVector(IndicatorRadius, IndicatorRadius, 200.f);
+	//	AimDecal->SetRelativeRotation(FRotator(-90.f, 0.f, 0.f)); // ¹Ù´Ú¿¡ ´¯È÷±â
+	//}
+}
+
+void UGA_AimItem::UpdateAim()
+{
+	//if (!AimDecal) return;
+	//
+	//AActor* Avatar = GetAvatarActorFromActorInfo();
+	//if (!Avatar) return;
+	//
+	//FVector Start = Avatar->GetActorLocation() + Avatar->GetActorForwardVector() * 100.f;
+	//FVector LaunchVelocity = Avatar->GetActorForwardVector() * 30.f;
+	//
+	//FPredictProjectilePathParams PathParams;
+	//PathParams.StartLocation = Start;
+	//PathParams.LaunchVelocity = LaunchVelocity;
+	//PathParams.OverrideGravityZ = GetWorld()->GetGravityZ();
+	//PathParams.ProjectileRadius = 5.f;
+	//PathParams.SimFrequency = 15.f;
+	//PathParams.MaxSimTime = 3.f;
+	//PathParams.TraceChannel = ECC_Visibility;
+	//
+	//FPredictProjectilePathResult PathResult;
+	//if (UGameplayStatics::PredictProjectilePath(this, PathParams, PathResult))
+	//{
+	//	AimDecal->SetWorldLocation(PathResult.HitResult.Location);
+	//}
+}
+
+void UGA_AimItem::EndAbility(const FGameplayAbilitySpecHandle Handle,
+    const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo,
+    bool bReplicateEndAbility, bool bWasCancelled)
+{
+    if (AimDecal)
+    {
+        AimDecal->DestroyComponent();
+        AimDecal = nullptr;
+    }
+
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
--- Project/Feature/LogOutPlayer/Source/New_ThePhantomTwins/GA/Action/GA_AimItem.h
+++ Project/Feature/LogOutPlayer/Source/New_ThePhantomTwins/GA/Action/GA_AimItem.h
@@ -16,11 +16,24 @@ class NEW_THEPHANTOMTWINS_API UGA_AimItem : public UGameplayAbility
 	GENERATED_BODY()
 public:
 	UGA_AimItem();
+
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	void UpdateAim();
+	void EndAbility(FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	                FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
 	TObjectPtr<UAnimMontage> HoldingItemMontage;
 	UPROPERTY()
 	UAbilityTask_PlayMontageAndWait* PlayHoldingItemMontageTask = nullptr;
+
+	UPROPERTY()
+	UDecalComponent* AimDecal;
+
+	UPROPERTY(EditAnywhere, Category = "Aim")
+	UMaterialInterface* DecalMaterial;
+
+	UPROPERTY(EditAnywhere, Category = "Aim")
+	float IndicatorRadius = 200.f; // 떨어질 때 반경
 };
