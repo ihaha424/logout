72d868271c9b1233eb504810422ff34b565a9aa0
6P_kimkaeun <6P_kimkaeun@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-12 05:46:44 +0000

    Player 포물선 코드 커밋
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@1945 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/Player/Source/New_ThePhantomTwins/GA/Action/GA_AimItem.cpp
+++ Project/Feature/Player/Source/New_ThePhantomTwins/GA/Action/GA_AimItem.cpp
@@ -2,13 +2,13 @@
 
 
 #include "GA_AimItem.h"
-
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "Log/TPTLog.h"
 #include "Tags/TPTGameplayTags.h"
-#include "Components/DecalComponent.h"
+#include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "GameFramework/ProjectileMovementComponent.h"
+#include "Net/RepLayout.h"
 
 UGA_AimItem::UGA_AimItem()
 {
@@ -25,41 +25,107 @@ void UGA_AimItem::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const
 	PlayHoldingItemMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("HoldingItemMontage"), HoldingItemMontage, 1.0f);
 	PlayHoldingItemMontageTask->ReadyForActivation();
 
-	//
-	//if (AActor* Avatar = GetAvatarActorFromActorInfo())
-	//{
-	//	AimDecal = NewObject<UDecalComponent>(Avatar);
-	//	AimDecal->RegisterComponent();
-	//	AimDecal->SetDecalMaterial(DecalMaterial);
-	//	AimDecal->DecalSize = FVector(IndicatorRadius, IndicatorRadius, 200.f);
-	//	AimDecal->SetRelativeRotation(FRotator(-90.f, 0.f, 0.f)); // 바닥에 눕히기
-	//}
+    OwnerActor = ActorInfo->AvatarActor.Get();
+    NULLCHECK_CODE_RETURN_LOG(OwnerActor, GALog, Warning, EndAbility(Handle, ActorInfo, ActivationInfo, false, false);, )
+
+    // Spline 컴포넌트 생성 후 플레이어에 붙임
+	SplineComp = NewObject<USplineComponent>(OwnerActor, USplineComponent::StaticClass(), TEXT("AimSpline"));
+    // 플레이어로부터 메쉬 컴포넌트 찾기
+	OwnerMeshComp = OwnerActor->FindComponentByClass<UStaticMeshComponent>();
+    NULLCHECK_CODE_RETURN_LOG(OwnerMeshComp, GALog, Warning, EndAbility(Handle, ActorInfo, ActivationInfo, false, false); , )
+
+    // 소켓을 부모로 설정
+	SplineComp->SetupAttachment(OwnerMeshComp, TEXT("RightHand"));
+    SplineComp->RegisterComponent();
+
+	// 주기적 위치 업데이트
+	GetWorld()->GetTimerManager().SetTimer(
+		UpdateTimerHandle,
+		this,
+		&UGA_AimItem::Updateparabola,
+		0.5f,
+		true
+	);
 }
 
-void UGA_AimItem::UpdateAim()
+void UGA_AimItem::Updateparabola()
 {
-	//if (!AimDecal) return;
-	//
-	//AActor* Avatar = GetAvatarActorFromActorInfo();
-	//if (!Avatar) return;
-	//
-	//FVector Start = Avatar->GetActorLocation() + Avatar->GetActorForwardVector() * 100.f;
-	//FVector LaunchVelocity = Avatar->GetActorForwardVector() * 30.f;
-	//
-	//FPredictProjectilePathParams PathParams;
-	//PathParams.StartLocation = Start;
-	//PathParams.LaunchVelocity = LaunchVelocity;
-	//PathParams.OverrideGravityZ = GetWorld()->GetGravityZ();
-	//PathParams.ProjectileRadius = 5.f;
-	//PathParams.SimFrequency = 15.f;
-	//PathParams.MaxSimTime = 3.f;
-	//PathParams.TraceChannel = ECC_Visibility;
-	//
-	//FPredictProjectilePathResult PathResult;
-	//if (UGameplayStatics::PredictProjectilePath(this, PathParams, PathResult))
-	//{
-	//	AimDecal->SetWorldLocation(PathResult.HitResult.Location);
-	//}
+    NULLCHECK_CODE_RETURN_LOG(OwnerActor, GALog, Warning, EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false); , )
+    NULLCHECK_CODE_RETURN_LOG(OwnerMeshComp, GALog, Warning, EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false);, )
+    NULLCHECK_CODE_RETURN_LOG(SplineStaticMesh, GALog, Warning, EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false);, )
+    NULLCHECK_CODE_RETURN_LOG(SplineMaterial, GALog, Warning, EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false);, )
+
+    // 기존 스플라인 & 메쉬 초기화
+    SplineComp->ClearSplinePoints(false);
+    for (auto* Mesh : SplineMeshes)
+    {
+        if (Mesh) Mesh->DestroyComponent();
+    }
+    SplineMeshes.Empty();
+
+    // 시작 위치 & 발사 방향
+    FVector StartLocation = OwnerMeshComp->GetSocketLocation(TEXT("RightHand"));
+    //TPT_LOG(GALog, Warning, TEXT("%d, %d, %d"), StartLocation.X, StartLocation.Y, StartLocation.Z);
+    FVector ForwardVector = OwnerActor->GetActorForwardVector() * 1000.f;
+
+    // 포물선 설정
+    FPredictProjectilePathParams Params;
+    Params.StartLocation = StartLocation;
+    Params.LaunchVelocity = ForwardVector;
+    Params.bTraceWithCollision = true;
+    Params.ProjectileRadius = 5.f;
+    Params.MaxSimTime = 2.f;
+    Params.SimFrequency = 15.f;
+    Params.TraceChannel = ECollisionChannel::ECC_Visibility;
+    Params.ActorsToIgnore.Add(OwnerActor);
+
+    FPredictProjectilePathResult Result;
+    bool bHit = UGameplayStatics::PredictProjectilePath(OwnerActor, Params, Result);
+
+    // 스플라인 포인트 추가
+    for (const FPredictProjectilePathPointData& PointData : Result.PathData)
+    {
+        SplineComp->AddSplinePoint(PointData.Location, ESplineCoordinateSpace::World, false);
+    }
+    SplineComp->UpdateSpline();
+
+    // 스플라인 메쉬 생성
+    int32 NumSegments = SplineComp->GetNumberOfSplinePoints() - 1;
+
+    for (int32 i = 0; i < NumSegments; i++)
+    {
+        FVector StartPos = SplineComp->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World);
+        FVector StartTan = SplineComp->GetTangentAtSplinePoint(i, ESplineCoordinateSpace::World);
+        FVector EndPos = SplineComp->GetLocationAtSplinePoint(i + 1, ESplineCoordinateSpace::World);
+        FVector EndTan = SplineComp->GetTangentAtSplinePoint(i + 1, ESplineCoordinateSpace::World);
+
+        USplineMeshComponent* NewMesh = NewObject<USplineMeshComponent>(OwnerActor);
+        if (!NewMesh) continue;
+
+        // 기본 세팅
+        NewMesh->SetMobility(EComponentMobility::Movable);
+        NewMesh->SetForwardAxis(ESplineMeshAxis::Z);
+        NewMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        NewMesh->SetStartScale(FVector2D(0.03f, 0.03f));
+        NewMesh->SetEndScale(FVector2D(0.03f, 0.03f));
+        NewMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        NewMesh->SetCastShadow(false);
+        NewMesh->bCastDynamicShadow = false;
+        NewMesh->bCastStaticShadow = false;
+    	NewMesh->SetStaticMesh(SplineStaticMesh);
+
+        // 스플라인에 붙이기
+        NewMesh->AttachToComponent(SplineComp, FAttachmentTransformRules::KeepWorldTransform);
+        NewMesh->RegisterComponent();
+
+        // Start-End 적용
+        NewMesh->SetStartAndEnd(StartPos, StartTan, EndPos, EndTan, true);
+
+        // 머테리얼은 반드시 마지막에
+    	NewMesh->SetMaterial(0, SplineMaterial);
+
+        SplineMeshes.Add(NewMesh);
+    }
 }
 
 void UGA_AimItem::EndAbility(const FGameplayAbilitySpecHandle Handle,
@@ -67,11 +133,30 @@ void UGA_AimItem::EndAbility(const FGameplayAbilitySpecHandle Handle,
     const FGameplayAbilityActivationInfo ActivationInfo,
     bool bReplicateEndAbility, bool bWasCancelled)
 {
-    if (AimDecal)
+	GetWorld()->GetTimerManager().ClearTimer(UpdateTimerHandle);
+
+    NULLCHECK_CODE_RETURN_LOG(SplineComp, GALog, Warning, EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false, false); , );
+
+	SplineComp->ClearSplinePoints(true);
+
+    if (SplineMeshes.Num() > 0)
     {
-        AimDecal->DestroyComponent();
-        AimDecal = nullptr;
+        for (int32 i = 0; i < SplineMeshes.Num(); i++)
+        {
+            if (SplineMeshes[i])
+            {
+                SplineMeshes[i]->DestroyComponent();
+            }
+        }
+
+        SplineMeshes.Empty();
     }
+
+	if (SplineComp)
+	{
+        SplineComp->DestroyComponent();
+        SplineComp = nullptr;
+	}
 
     Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
