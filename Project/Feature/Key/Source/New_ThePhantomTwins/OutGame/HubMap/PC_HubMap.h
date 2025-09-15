750d231718f3256c5118f12899d8bd628d06cecf
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-15 02:25:42 +0000

    [Key] Develop->Key merge
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@2000 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/Key/Source/New_ThePhantomTwins/OutGame/HubMap/PC_HubMap.h
+++ Project/Feature/Key/Source/New_ThePhantomTwins/OutGame/HubMap/PC_HubMap.h
@@ -5,14 +5,32 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "UIManagerPlayerController.h"
+#include "OutGame/HubMap/GS_HubMap.h"
+#include "Data/MapType.h"
 #include "PC_HubMap.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class NEW_THEPHANTOMTWINS_API APC_HubMap : public AUIManagerPlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable, Server, Reliable, Category = "HubMap | Character Seletect")
+	void C2S_SelectChractor(const ECharacterType type);
+	void C2S_SelectChractor_Implementation(const ECharacterType type);
+
+	UFUNCTION(BlueprintCallable, Server, Reliable, Category = "HubMap | State Seletect")
+	void C2S_SelectState(const EHubMapState type);
+	void C2S_SelectState_Implementation(const EHubMapState type);
+
+	UFUNCTION(BlueprintCallable, Server, Reliable, Category = "HubMap | Map Seletect")
+	void C2S_SelectMap(const FName& type, EMapType State);
+	void C2S_SelectMap_Implementation(const FName& type, EMapType State);
+
+	UFUNCTION(BlueprintCallable, Server, Reliable, Category = "HubMap | LevelTravel")
+	void C2S_LevelTravel(const FName& type, bool bAbsolute);
+	void C2S_LevelTravel_Implementation(const FName& type, bool bAbsolute);
 };
