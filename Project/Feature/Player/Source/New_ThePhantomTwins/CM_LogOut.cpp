96691ff16549fffaf61ef6ff03d2de3ff46a749e
6P_kimkaeun <6P_kimkaeun@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-18 03:38:49 +0000

    CheatManager 명령어 추가
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@2208 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Feature/Player/Source/New_ThePhantomTwins/CM_LogOut.cpp
+++ Project/Feature/Player/Source/New_ThePhantomTwins/CM_LogOut.cpp
@@ -5,57 +5,134 @@
 #include "Player/PlayerCharacter.h"
 #include "Attribute/PlayerAttributeSet.h"
 #include "GameFramework/PlayerController.h"
-
+#include "Player/PC_Player.h"
+#include "Player/PS_Player.h"
+#include "GS_PhantomTwins.h"
+#include "Engine/World.h"
+#include "Log/TPTLog.h"
+#include "Objects/InventoryComponent.h"
 
 void UCM_LogOut::SetHP(float hp)
 {
-	if (APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn()))
+	APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn());
+	NULLCHECK_RETURN_LOG(Character, CMLog, Warning, );
+
+	UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent();
+	NULLCHECK_RETURN_LOG(ASC, CMLog, Warning, );
+
+	if (const UPlayerAttributeSet* AttributeSet = Character->GetAbilitySystemComponent()->GetSet<UPlayerAttributeSet>())
 	{
-		if (UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent())
-		{
-			if (const UPlayerAttributeSet* AttributeSet = Character->GetAbilitySystemComponent()->GetSet<UPlayerAttributeSet>())
-			{
-				ASC->SetNumericAttributeBase(UPlayerAttributeSet::GetHPAttribute(), hp);
-			}
-		}
+		ASC->SetNumericAttributeBase(UPlayerAttributeSet::GetHPAttribute(), hp);
 	}
 }
 
-void UCM_LogOut::SetMaxHP()
+void UCM_LogOut::FullHP()
 {
-	if (APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn()))
+	APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn());
+	NULLCHECK_RETURN_LOG(Character, CMLog, Warning, );
+
+	UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent();
+	NULLCHECK_RETURN_LOG(ASC, CMLog, Warning, );
+
+	if (const UPlayerAttributeSet* AttributeSet = Character->GetAbilitySystemComponent()->GetSet<UPlayerAttributeSet>())
 	{
-		if (UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent())
-		{
-			if (const UPlayerAttributeSet* AttributeSet = Character->GetAbilitySystemComponent()->GetSet<UPlayerAttributeSet>())
-			{
-				ASC->SetNumericAttributeBase(UPlayerAttributeSet::GetHPAttribute(), AttributeSet->GetMaxHP());
-			}
-		}
+		ASC->SetNumericAttributeBase(UPlayerAttributeSet::GetHPAttribute(), AttributeSet->GetMaxHP());
+	}
+}
+
+void UCM_LogOut::SetMental(float mental)
+{
+	APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn());
+	NULLCHECK_RETURN_LOG(Character, CMLog, Warning, );
+
+	UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent();
+	NULLCHECK_RETURN_LOG(ASC, CMLog, Warning, );
+
+	if (const UPlayerAttributeSet* AttributeSet = Character->GetAbilitySystemComponent()->GetSet<UPlayerAttributeSet>())
+	{
+		ASC->SetNumericAttributeBase(UPlayerAttributeSet::GetMaxMentalPointAttribute(), mental);
+	}
+}
+
+void UCM_LogOut::FullMental()
+{
+	APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn());
+	NULLCHECK_RETURN_LOG(Character, CMLog, Warning, );
+
+	UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent();
+	NULLCHECK_RETURN_LOG(ASC, CMLog, Warning, );
+
+	if (const UPlayerAttributeSet* AttributeSet = Character->GetAbilitySystemComponent()->GetSet<UPlayerAttributeSet>())
+	{
+		ASC->SetNumericAttributeBase(UPlayerAttributeSet::GetMaxMentalPointAttribute(), AttributeSet->GetMaxMentalPoint());
 	}
 }
 
 void UCM_LogOut::SetRecovery(bool bFlag)
 {
-	if (APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn()))
+	APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn());
+	NULLCHECK_RETURN_LOG(Character, CMLog, Warning, );
+
+	UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent();
+	NULLCHECK_RETURN_LOG(ASC, CMLog, Warning, );
+	
+	if (bFlag)
 	{
-		if (UAbilitySystemComponent* ASC = Character->GetAbilitySystemComponent())
+		if (Character->GetClass()->ImplementsInterface(UInteract::StaticClass()))
 		{
-			if (bFlag)
+			if (Character->HasAuthority())
 			{
-				if (Character->GetClass()->ImplementsInterface(UInteract::StaticClass()))
-				{
-					if (Character->HasAuthority())
-					{
-						IInteract::Execute_OnInteractServer(Character, Character);
-					}
-					IInteract::Execute_OnInteractClient(Character, Character);
-				}
+				IInteract::Execute_OnInteractServer(Character, Character);
 			}
-			else
-			{
-				SetHP(0);
-			}
+			IInteract::Execute_OnInteractClient(Character, Character);
 		}
 	}
+	else
+	{
+		SetHP(0);
+	}
 }
+
+void UCM_LogOut::GiveAllData()
+{
+	AGS_PhantomTwins* GS = GetWorld()->GetGameState<AGS_PhantomTwins>();
+	NULLCHECK_RETURN_LOG(GS, CMLog, Warning, );
+
+	GS->AddCollectedItem(GetPlayerController()->GetPawn(), 5);
+}
+
+void UCM_LogOut::MoveToExit()
+{
+	GetPlayerController()->GetPawn()->SetActorLocation(ExitPosition);
+}
+
+void UCM_LogOut::StartST1()
+{
+
+}
+
+void UCM_LogOut::StartST2()
+{
+
+}
+
+void UCM_LogOut::ClearNow()
+{
+
+}
+
+void UCM_LogOut::GiveItems()
+{
+	APlayerCharacter* Character = Cast<APlayerCharacter>(GetPlayerController()->GetPawn());
+	NULLCHECK_RETURN_LOG(Character, CMLog, Warning, );
+
+	APS_Player* PS = Character->GetPlayerState<APS_Player>();
+	NULLCHECK_RETURN_LOG(PS, CMLog, Warning, );
+
+	for (int32 i = 0; i < 2; i++)
+	{
+		PS->InventoryComp->AddItem(EItemType::EMP);
+		PS->InventoryComp->AddItem(EItemType::NoiseBomb);
+	}
+}
+
