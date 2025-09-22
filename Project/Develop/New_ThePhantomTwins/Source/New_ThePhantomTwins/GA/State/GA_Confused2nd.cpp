2a33b8452733c707501461371bc78272d5ab44cc
6P_hamshina <6P_hamshina@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-09-22 08:47:39 +0000

    [HotFix] 사운드 두개를 넣을수 있도록 수정
    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@2360 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/GA/State/GA_Confused2nd.cpp
+++ Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/GA/State/GA_Confused2nd.cpp
@@ -28,9 +28,13 @@ void UGA_Confused2nd::ActivateAbility(const FGameplayAbilitySpecHandle Handle, c
 
 	if (ActorInfo && ActorInfo->IsLocallyControlled())
 	{
-		if (USoundBase* Sound = SoundCue) // SoundCue는 클래스에 UPROPERTY로 선언되어 있어야 함
+		if (USoundBase* Sound = SoundCue1st) // SoundCue는 클래스에 UPROPERTY로 선언되어 있어야 함
 		{
-			ActiveAudioComponent = UGameplayStatics::SpawnSoundAttached(Sound, ActorInfo->AvatarActor->GetRootComponent());
+			ActiveAudioComponent1st = UGameplayStatics::SpawnSoundAttached(Sound, ActorInfo->AvatarActor->GetRootComponent());
+		}
+		if (USoundBase* Sound = SoundCue2nd) // SoundCue는 클래스에 UPROPERTY로 선언되어 있어야 함
+		{
+			ActiveAudioComponent2nd = UGameplayStatics::SpawnSoundAttached(Sound, ActorInfo->AvatarActor->GetRootComponent());
 		}
 	}
 }
@@ -39,10 +43,15 @@ void UGA_Confused2nd::EndAbility(const FGameplayAbilitySpecHandle Handle, const 
 {
 	if (ActorInfo && ActorInfo->IsLocallyControlled())
 	{
-		if (ActiveAudioComponent)
+		if (ActiveAudioComponent1st)
 		{
-			ActiveAudioComponent->Stop();
-			ActiveAudioComponent = nullptr;
+			ActiveAudioComponent1st->Stop();
+			ActiveAudioComponent1st = nullptr;
+		}
+		if (ActiveAudioComponent2nd)
+		{
+			ActiveAudioComponent2nd->Stop();
+			ActiveAudioComponent2nd = nullptr;
 		}
 	}
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
