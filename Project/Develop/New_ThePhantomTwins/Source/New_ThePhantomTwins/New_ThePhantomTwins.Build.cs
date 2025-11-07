add7e50f4a5c9d268d7e2d953ac4b42136ac96dc
6P_songchaelin <6P_songchaelin@323376ff-746e-4b47-8525-04d7babb2b8c>
2025-11-07 07:45:17 +0000

    git-svn-id: https://svna.gameinjae.kr/svn/GA6thFinal_Time@4274 323376ff-746e-4b47-8525-04d7babb2b8c

--- Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/New_ThePhantomTwins.Build.cs
+++ Project/Develop/New_ThePhantomTwins/Source/New_ThePhantomTwins/New_ThePhantomTwins.Build.cs
@@ -23,7 +23,8 @@ public class New_ThePhantomTwins : ModuleRules
             "Sockets",
             "Networking",
             "Niagara",
-            "GeometryCache"
+            "GeometryCache",
+            "NetCore"
 
         });
 
